02c163f4424c9255a09ececb180ee2c2cb07f118
Nwalozie <iyecknwalozie@gmail.com>
2024-01-30 04:32:43 +0100

    Basic setup

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,9 @@
+// src/indec.js
+import $ from 'jquery';
+
+$(document).ready(function() {
+  // Adds three paragraphs to the page body
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+});
